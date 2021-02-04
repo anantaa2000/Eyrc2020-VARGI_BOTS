ff52d29c50467a19988c82022c87137b69463c3a
ANANT AGARWAL <46123373+anantaa2000@users.noreply.github.com>
2021-02-04 19:02:09 +0530

    Update iot.py

--- pkg_ros_iot_bridge/scripts/pyiot/iot.py
+++ pkg_ros_iot_bridge/scripts/pyiot/iot.py
@@ -40,17 +40,48 @@ def mqtt_subscribe_thread_start(arg_callback_func, arg_broker_url, arg_broker_po
     except:
         return -1
 
-def http_send_spread(id, final_x, final_y, final_theta):
-    url = "https://script.google.com/macros/s/AKfycbzOo4pSZ-JHofkbW7-hDTQRib6OJ-DpYTnfmO_uszuqhVH1pMg/exec"
+def http_send_spread_inventory(data):
+    url = "https://script.google.com/macros/s/AKfycbxQ8X4Lu1aTrL4uoiuZ1WNIB91gNss29FE7IZhiV3U-AQjPlWI/exec"
+    url2 = "https://script.google.com/macros/s/AKfycbw5xylppoda-8HPjt2Tzq4ShU_Xef-Ik-hEtBPcPk0gdGw8095j4RZ7/exec"
+    
 
-
-    url2 = "https://script.google.com/macros/s/AKfycbygyuHa3Z-ObngJexz4yhCXILVuZ_7nUnNoK8rLuZapLCiMYbPI/exec" # isko yaha place krna h 
-    #https://script.google.com/macros/s/AKfycbw850dk4moVgebU2GGe0PUQUvvg8jTpSjBQCawJt3_13vgujLk/exec   # ye unki id h 88888888888888888888888
-
-    parameter = {"id":id, "final_x":final_x, "final_y":final_y, "final_theta":final_theta}
-    parameter_eyrc = {"id":"task1", "team_id":"VB_0528", "unique_id":"JaiMataD", "turtle_x":final_x, "turtle_y":final_y, "turtle_theta":final_theta}
+    parameter = {"id":"Inventory", "Team Id":"VB_0528", "Unique Id":"JaiMataD", "SKU":data[1], "Item":data[2], "Priority":data[3], "Storage Number":data[4], "Cost":data[5], "Quantity":data[6]}
     response = requests.get(url, params = parameter)
-    response2 = requests.get(url2, params = parameter_eyrc)
+    response2 = requests.get(url2, params = parameter)
+    print response
+    print response2
+
+def http_send_spread_incoming_orders(data):
+    url = "https://script.google.com/macros/s/AKfycbxQ8X4Lu1aTrL4uoiuZ1WNIB91gNss29FE7IZhiV3U-AQjPlWI/exec"
+    url2 = "https://script.google.com/macros/s/AKfycbw5xylppoda-8HPjt2Tzq4ShU_Xef-Ik-hEtBPcPk0gdGw8095j4RZ7/exec"
+
+    parameter = {"id":"IncomingOrders", "Team Id":"VB_0528", "Unique Id":"JaiMataD", "Order ID":data[1],"Order Date and Time":data[2], "Item":data[3], "Priority":data[4], "Order Quantity":data[5], "City":data[6], "Longitude":data[7], "Latitude":data[8], "Cost":data[9]}
+    response = requests.get(url, params = parameter)
+    response2 = requests.get(url2, params = parameter)
+    print response
+    print response2
+
+
+def http_send_spread_orders_dispatched(data):
+    url = "https://script.google.com/macros/s/AKfycbxQ8X4Lu1aTrL4uoiuZ1WNIB91gNss29FE7IZhiV3U-AQjPlWI/exec"
+    url2 = "https://script.google.com/macros/s/AKfycbw5xylppoda-8HPjt2Tzq4ShU_Xef-Ik-hEtBPcPk0gdGw8095j4RZ7/exec"
+
+    parameter = {"id":"OrdersDispatched", "Team Id":"VB_0528", "Unique Id":"JaiMataD", "Order ID":data[1], "City":data[2], "Item":data[3], "Priority":data[4], "Dispatch Quantity":data[5], "Cost":data[6],"Dispatch Status":data[7], "Dispatch Date and Time":data[8]}
+    response = requests.get(url, params = parameter)
+    response2 = requests.get(url2, params = parameter)
+    print response
+    print response2
+
+
+def http_send_spread_orders_shipped(data):
+    url = "https://script.google.com/macros/s/AKfycbxQ8X4Lu1aTrL4uoiuZ1WNIB91gNss29FE7IZhiV3U-AQjPlWI/exec"
+    url2 = "https://script.google.com/macros/s/AKfycbw5xylppoda-8HPjt2Tzq4ShU_Xef-Ik-hEtBPcPk0gdGw8095j4RZ7/exec"
+
+    parameter = {"id":"OrderShipped", "Team Id":"VB_0528", "Unique Id":"JaiMataD", "Order ID":data[1], "City":data[2], "Item":data[3], "Priority":data[4], "Shipped Quantity":data[5], "Cost":data[6],"Shipped Status":data[7], "Shipped Date and Time":data[8], "Estimated Time of Delivery":data[9]}
+    response = requests.get(url, params = parameter)
+    # response2 = requests.get(url2, params = parameter)
+    print response
+    # print response2
 
 
 
@@ -60,14 +91,25 @@ def mqtt_publish(arg_broker_url, arg_broker_port, arg_mqtt_topic, arg_mqtt_messa
         mqtt_client = mqtt.Client("mqtt_pub")
         mqtt_client.connect(arg_broker_url, arg_broker_port)
         mqtt_client.loop_start()
-
         print("Publishing message to topic", arg_mqtt_topic)
         mqtt_client.publish(arg_mqtt_topic, str(arg_mqtt_message), arg_mqtt_qos)
-        http_parameters = {"id":arg_spreadsheet_id, "final_x": arg_mqtt_message[0], "final_y":arg_mqtt_message[1], "final_theta":arg_mqtt_message[2]}
-        http_send_spread(**http_parameters)
-        time.sleep(2)
 
-        time.sleep(0.1) # wait
+
+        if arg_mqtt_message[0]=="Inventory":
+            http_send_spread_inventory(arg_mqtt_message)
+
+        if arg_mqtt_message[0]=="IncomingOrders":
+            http_send_spread_incoming_orders(arg_mqtt_message)
+
+        if arg_mqtt_message[0]=="OrdersDispatched":
+            http_send_spread_orders_dispatched(arg_mqtt_message)
+
+        if arg_mqtt_message[0]=="OrderShipped":
+            http_send_spread_orders_shipped(arg_mqtt_message)
+
+
+        
+        time.sleep(2)
 
         mqtt_client.loop_stop() #stop the loop
         return 0
